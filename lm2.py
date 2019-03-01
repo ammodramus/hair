22d68757e5ff46b327ff14cec436e36f527515f1
Peter Wilton <peterrwilton@gmail.com>
2019-03-01 14:28:08 -0800

    bugfixes for dealing with freqs @ margins

--- lm2.py
+++ lm2.py
@@ -21,6 +21,16 @@ mp.dps = 20
 
 @jit
 def convolve_binom_and_beta_sum(x, n, p):
+    if p == 0.0 or p == 1.0:
+        if x > 0 and x < 1:
+            return 0.0
+    if (p == 1.0 and x == 1.0) or (p == 0.0 and x == 0.0):
+        return 1.0
+    if x == 0.0:
+        return st.binom.pmf(0, n, p)
+    elif x == 1.0:
+        return st.binom.pmf(n, n, p)
+
     logterms = np.zeros(n+1)
     for j in range(1, n):
         logterms[j] = scipy.stats.binom.logpmf(j, n, p) + scipy.stats.beta.logpdf(x, j, (n-j))
@@ -31,14 +41,30 @@ def convolve_binom_and_beta_sum(x, n, p):
 
 @jit
 def convolve_binom_and_beta(x, n, p):
+    if p == 0.0 or p == 1.0:
+        if x > 0 and x < 1:
+            return 0.0
+    if (p == 1.0 and x == 1.0) or (p == 0.0 and x == 0.0):
+        return 1.0
+    if x == 0.0:
+        return st.binom.pmf(0, n, p)
+    elif x == 1.0:
+        return st.binom.pmf(n, n, p)
     prob = -n*(n-1)*(p-1)*p*((p-1)*(x-1))**(n-2)*scipy.special.hyp2f1(1-n, 2-n, 2, x*p/((x-1)*(p-1)))
     if np.isnan(prob):
         prob = float(-n*(n-1)*(p-1)*p*((p-1)*(x-1))**(n-2)*mp.hyp2f1(1-n, 2-n, 2, x*p/((x-1)*(p-1))))
     return prob
 
 def log_convolve_binom_and_beta_mp(x, n, p):
-    if x == 0 or x == 1.0 or p == 0.0 or p == 1.0:
-        return -np.inf
+    if p == 0.0 or p == 1.0:
+        if x > 0 and x < 1:
+            return -np.inf
+    if (p == 1.0 and x == 1.0) or (p == 0.0 and x == 0.0):
+        return 0.0
+    if x == 0.0:
+        return st.binom.logpmf(0, n, p)
+    elif x == 1.0:
+        return st.binom.logpmf(n, n, p)
     n = mp.mpf(n)
     p = mp.mpf(p)
     x = mp.mpf(x)
@@ -55,6 +81,7 @@ def log_convolve_binom_and_beta(x, n, p):
         return st.binom.logpmf(0, n, p)
     elif x == 1.0:
         return st.binom.logpmf(n, n, p)
+
     logprob = np.log(-n*(n-1)*(p-1)*p*((p-1)*(x-1))**(n-2)*scipy.special.hyp2f1(1-n, 2-n, 2, x*p/((x-1)*(p-1))))
     if not np.isfinite(logprob):
         return log_convolve_binom_and_beta_mp(x, n, p)
@@ -71,26 +98,21 @@ def log_likelihood_model_B(params, hair_freqs, blood_freqs, cheek_freqs):
     total_loglike = 0.0
     for hair_i, cheek_i, blood_i in zip(hair_freqs, cheek_freqs, blood_freqs):
         s = f*blood_i + (1-f)*cheek_i
-        indiv_hair_logprobs = []
+        freq_n1_logprobs = []
         for k in range(0,int(N1)+1):
             log_binom_prob = st.binom.logpmf(k, N1, s)
-            logprob = log_binom_prob   # the logprobability for this outcome of the first bottleneck
+            freq_logprob = log_binom_prob   # the logprobability for this outcome of the first bottleneck
             p_B1 = float(k)/N1
             for hair_ij in hair_i:
-                #if hair_ij == 0.0:
-                #    hair_ij += 1e-5
-                #elif hair_ij == 1.0:
-                #    hair_ij -= 1e-5
                 val = log_convolve_binom_and_beta(hair_ij, N2, p_B1)
                 if val == -np.inf and p_B1 > 0 and p_B1 < 1:
-                    print hair_ij, p_B1, val
-                logprob += val
-            indiv_hair_logprobs.append(logprob)
-        #if np.all(np.array(indiv_hair_logprobs) == -np.inf):
-        #    import pdb; pdb.set_trace()
-        indiv_loglike = scipy.special.logsumexp(indiv_hair_logprobs)
+                    print 'bad convolution:', hair_ij, N2, p_B1, '--', val
+                freq_logprob += val
+            freq_n1_logprobs.append(freq_logprob)
+        indiv_loglike = scipy.special.logsumexp(freq_n1_logprobs)
         total_loglike += indiv_loglike
     return total_loglike
+
 
 def log_likelihood_model_B_array(params, hair_freqs, blood_freqs, cheek_freqs):
     tup_params = tuple(params)
@@ -112,6 +134,12 @@ def log_likelihood_model_B_two_bots(params, *args):
     
     frac_b_N1 = (N1-N1a)/(N1b-N1a)
     frac_b_N2 = (N2-N2a)/(N2b-N2a)
+
+    if np.isnan(frac_b_N1):
+        frac_b_N1 = 1.0
+    if np.isnan(frac_b_N2):
+        frac_b_N2 = 1.0
+
     loglike_aa = log_likelihood_model_B_array((f, N1a, N2a), *args)
     loglike_ab = log_likelihood_model_B_array((f, N1a, N2b), *args)
     loglike_ba = log_likelihood_model_B_array((f, N1b, N2a), *args)
